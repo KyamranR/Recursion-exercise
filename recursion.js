42cbb405ac74c567eec9558f6cb59384ea84a3df
Kyamran Rizayev <kyamranr@gmail.com>
2024-08-19 14:18:51 -0400

    finished recursion.js

--- recursion.js
+++ recursion.js
@@ -1,50 +1,105 @@
 /** product: calculate the product of an array of numbers. */
 
 function product(nums) {
-
+  if (nums.length === 0) {
+    return 1;
+  } else {
+    return nums[0] * product(nums.slice(1));
+  }
 }
 
 /** longest: return the length of the longest word in an array of words. */
 
 function longest(words) {
-
+  if (words.length === 0) {
+    return 0;
+  } else {
+    const firstWord = words[0].length;
+    const restLongest = longest(words.slice(1));
+    return Math.max(firstWord, restLongest);
+  }
 }
 
 /** everyOther: return a string with every other letter. */
 
 function everyOther(str) {
-
+  if (str.length === 0) {
+    return "";
+  } else {
+    return str[0] + everyOther(str.slice(2));
+  }
 }
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
 function isPalindrome(str) {
+  if (str.length <= 1) {
+    return true;
+  }
+  if (str[0] !== str[str.length - 1]) {
+    return false;
+  }
 
+  return isPalindrome(str.slice(1, str.length - 1));
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
-function findIndex(arr, val) {
+function findIndex(arr, val, index = 0) {
+  if (arr.length === 0) {
+    return -1;
+  }
 
+  if (arr[0] === val) {
+    return index;
+  }
+  return findIndex(arr.slice(1), val, index + 1);
 }
 
 /** revString: return a copy of a string, but in reverse. */
 
 function revString(str) {
+  if (str.length === 0) {
+    return "";
+  }
 
+  return str[str.length - 1] + revString(str.slice(0, -1));
 }
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
 function gatherStrings(obj) {
+  let result = [];
 
+  for (let key in obj) {
+    if (typeof obj[key] === "string") {
+      result.push(obj[key]);
+    } else if (typeof obj[key] === "object" && obj[key] !== null) {
+      result = result.concat(gatherStrings(obj[key]));
+    }
+  }
+  return result;
 }
 
 /** binarySearch: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
 
-function binarySearch(arr, val) {
+function binarySearch(arr, val, left = 0, right = arr.length - 1) {
+  if (left > right) {
+    return -1;
+  }
 
+  let middle = Math.floor((left + right) / 2);
+
+  if (arr[middle] === val) {
+    return middle;
+  }
+
+  if (val < arr[middle]) {
+    return binarySearch(arr, val, left, middle - 1);
+  }
+
+  return binarySearch(arr, val, middle + 1, right);
 }
 
 module.exports = {
@@ -55,5 +110,5 @@ module.exports = {
   findIndex,
   revString,
   gatherStrings,
-  binarySearch
+  binarySearch,
 };
